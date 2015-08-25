c33da70cd5134cc00d62f4b14eb95caee65b31ae
Maxime Bertonnier <m.bertonnier@gmail.com>
2015-08-25 10:15:18 +0200

    Update LaunchMyApp.js

--- www/android/LaunchMyApp.js
+++ www/android/LaunchMyApp.js
@@ -1,10 +1,12 @@
 (function () {
     "use strict";
 
-  var remainingAttempts = 10;
+  var remainingAttempts = 30;
 
   function waitForAndCallHandlerFunction(url) {
-    if (typeof window.handleOpenURL == "function") {
+    if (typeof window.handleOpenURL == "function"
+        && window.location.href === "http://meteor.local/") {
+            alert('handle !');
       window.handleOpenURL(url);
     } else if (remainingAttempts-- > 0) {
       setTimeout(function(){waitForAndCallHandlerFunction(url)}, 500);
