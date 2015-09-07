f01085fb4ef7b1e8bbd94a7a77a80303a2b36b53
Maxime Bertonnier <m.bertonnier@gmail.com>
2015-09-07 15:29:55 +0200

    fix(LaunchMyApp Android): hacky way to trigger on cold start with meteor

--- www/android/LaunchMyApp.js
+++ www/android/LaunchMyApp.js
@@ -1,24 +1,32 @@
-(function () {
+(function() {
     "use strict";
 
-  var remainingAttempts = 10;
+    var remainingAttempts = 30;
 
-  function waitForAndCallHandlerFunction(url) {
-    if (typeof window.handleOpenURL == "function") {
-      window.handleOpenURL(url);
-    } else if (remainingAttempts-- > 0) {
-      setTimeout(function(){waitForAndCallHandlerFunction(url)}, 500);
+    function waitForAndCallHandlerFunction(url) {
+        if (typeof window.handleOpenURL == "function" &&
+            window.location.href == "http://meteor.local/") {
+            console.log('handle open url');
+            window.handleOpenURL(url);
+        } else if (remainingAttempts-- > 0) {
+            setTimeout(function() {
+                waitForAndCallHandlerFunction(url)
+            }, 500);
+        }
     }
-  }
 
-  function triggerOpenURL() {
-    cordova.exec(
-        waitForAndCallHandlerFunction,
-        null,
-        "LaunchMyApp",
-        "checkIntent",
-        []);
-  }
+    function triggerOpenURL() {
+        console.log('triggerOpenURL ');
+        cordova.exec(
+            waitForAndCallHandlerFunction,
+            null,
+            "LaunchMyApp",
+            "checkIntent", []);
+    }
 
-  document.addEventListener("deviceready", triggerOpenURL, false);
+    document.addEventListener("deviceready", function() {
+        setTimeout(function() {
+            triggerOpenURL();
+        }, 5000);
+    }, false);
 }());
