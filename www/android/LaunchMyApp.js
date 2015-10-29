e58fa098b5996de42611b5d5fdbc4ecd63a52ee9
Maxime Bertonnier <m.bertonnier@gmail.com>
2015-10-29 16:27:19 +0100

    refactor(android): comment out android behaviour
    See :
    
    https://github.com/archriss/aix-app/commit/fcde57ecee0d82308b982fc91a1d8
    6cb0b85bef6

--- www/android/LaunchMyApp.js
+++ www/android/LaunchMyApp.js
@@ -1,30 +1,35 @@
-(function() {
-    "use strict";
+// Old code working on the fastest devices
+// Shouldn't be useful anymore thanks to this hack :
+// https://github.com/archriss/aix-app/commit/fcde57ecee0d82308b982fc91a1d86cb0b85bef6
 
-    var remainingAttempts = 30;
 
-    function waitForAndCallHandlerFunction(url) {
-        if (typeof window.handleOpenURL == "function" &&
-            window.location.href == "http://meteor.local/") {
-            window.handleOpenURL(url);
-        } else if (remainingAttempts-- > 0) {
-            setTimeout(function() {
-                waitForAndCallHandlerFunction(url)
-            }, 500);
-        }
-    }
-
-    function triggerOpenURL() {
-        cordova.exec(
-            waitForAndCallHandlerFunction,
-            null,
-            "LaunchMyApp",
-            "checkIntent", []);
-    }
-
-    document.addEventListener("deviceready", function() {
-        setTimeout(function() {
-            triggerOpenURL();
-        }, 500);
-    }, false);
-}());
+// (function() {
+//     "use strict";
+//
+//     var remainingAttempts = 30;
+//
+//     function waitForAndCallHandlerFunction(url) {
+//         if (typeof window.handleOpenURL == "function" &&
+//             window.location.href == "http://meteor.local/") {
+//             window.handleOpenURL(url);
+//         } else if (remainingAttempts-- > 0) {
+//             setTimeout(function() {
+//                 waitForAndCallHandlerFunction(url)
+//             }, 250);
+//         }
+//     }
+//
+//     function triggerOpenURL() {
+//         cordova.exec(
+//             waitForAndCallHandlerFunction,
+//             null,
+//             "LaunchMyApp",
+//             "checkIntent", []);
+//     }
+//
+//     document.addEventListener("deviceready", function() {
+//         setTimeout(function() {
+//             triggerOpenURL();
+//         }, 250);
+//     }, false);
+// }());
